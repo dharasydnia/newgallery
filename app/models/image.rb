6ea355af32b3e088ee264b5225fb4e4155c9577a
dharasydnia <djoshi@sydnia.com>
2012-05-02 12:01:14 +0530

    Initial commit

---
+++ app/models/image.rb
@@ -0,0 +1,3 @@
+class Image < ActiveRecord::Base
+  has_attached_file :img, :styles => { :thumb => "75x75", :small => "150x150" }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newgallery::Application.config.secret_token = 'bffd766649893a5d3b0f7969fdc8251fa7dda851872f1af0be968f565d8575f76492cb4634e14354f50437978321cc8c00ed39a772dd4940553ab21404757e56'
